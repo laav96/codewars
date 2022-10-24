c287ee65d23d44cdc4eec6e4849fa617fcb8dca8
Akeyla Virgin <a.virgin05@icloud.com>
2022-10-24 17:31:59 -0400

    solved string challenge

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+const str = "Hello World!"
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  };
+
+  console.log(str.toUpperCase());
